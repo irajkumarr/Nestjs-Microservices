1f41387940ec725706635b000f05f1465c9f1bcc
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2026-05-15 16:43:42 +0545

    feat(rider-service): implement order dispatching and RabbitMQ integration

--- rider-service/src/app.module.ts
+++ rider-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dispatch } from './entity/dispatch.entity';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         },
       }),
     }),
+    TypeOrmModule.forFeature([Dispatch]),
   ],
   controllers: [AppController],
   providers: [AppService],
